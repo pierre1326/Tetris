3d4e6d3df9fbed777605a29b4b7a33f4be6f2a04
pierre1326 <pierre1326@hotmail.com>
2019-03-07 12:14:55 -0600

    Menu
    Se crea por el completo el menu del juego. Se añade la activity para el desarollo del juego y se implementa un sistema para pausar el hilo en caso de salir de la activity del menu

--- app/src/main/java/com/tetris/pierre/tetris/GameActivity.java
+++ app/src/main/java/com/tetris/pierre/tetris/GameActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
 
+  private int width;
+  private int height;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_game);
+
+    width = getIntent().getIntExtra("width", 1080);
+    height = getIntent().getIntExtra("height", 1920);
   }
 }
--- app/src/main/java/com/tetris/pierre/tetris/Menu.java
+++ app/src/main/java/com/tetris/pierre/tetris/Menu.java
@@ -1,20 +1,29 @@
 package com.tetris.pierre.tetris;
 
 import android.app.ActivityOptions;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Point;
+import android.os.AsyncTask;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.transition.Explode;
 import android.transition.Fade;
 import android.transition.Slide;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import java.io.Console;
 
@@ -24,16 +33,23 @@ import Game.Util;
 
 public class Menu extends AppCompatActivity {
 
+  private Thread thread;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
     getWindow().setExitTransition(new Fade());
     setContentView(R.layout.activity_menu);
+
+    scaleView(findViewById(R.id.nameGame), 0f, 1f, 1000);
     scaleView(findViewById(R.id.iconGame), 0f, 1f, 1000);
     scaleView(findViewById(R.id.buttonStart), 0f, 1f, 1000);
     scaleView(findViewById(R.id.buttonExit), 0f, 1f, 1000);
     scaleView(findViewById(R.id.nameGame), 0f, 1f, 1000);
+
+    thread = new Thread(this);
+    thread.execute();
   }
 
   public void initGame(View view) {
@@ -61,6 +77,129 @@ public class Menu extends AppCompatActivity {
     Point size = new Point();
     display.getSize(size);
     return size;
+  }
+
+  @Override
+  public void onDestroy() {
+    if(thread != null) {
+      if(!thread.isCancelled()) {
+        thread.cancel(true);
+      }
+    }
+    super.onDestroy();
+  }
+
+  @Override
+  public void onPause() {
+    if(thread != null) {
+      thread.onPause();
+    }
+    super.onPause();
+  }
+
+  @Override
+  public void onResume() {
+    if(thread != null) {
+      thread.onResume();
+    }
+    super.onResume();
+  }
+
+  private class Thread extends AsyncTask<Void, Void, Void> {
+
+    private Context context;
+
+    private TextSwitcher textSwitcher;
+
+    private String[] texts = {"Tetris", "Tarea #1"};
+    private String[] colors = {"#FFFFFF", "#03A9F4"};
+
+    private int index = -1;
+    private int totalTexts = texts.length;
+
+    private Object pauseLock = new Object();
+    private boolean paused = false;
+
+    public Thread(Context context) {
+      this.context = context;
+      textSwitcher = findViewById(R.id.nameGame);
+      loadAnimations();
+      setFactory();
+    }
+
+    public void onPause() {
+      synchronized (pauseLock) {
+        paused = true;
+      }
+    }
+
+    public void onResume() {
+      synchronized (pauseLock) {
+        paused = false;
+        pauseLock.notifyAll();
+      }
+    }
+
+    private void loadAnimations() {
+      Animation in = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+      Animation out = AnimationUtils.loadAnimation(context, android.R.anim.slide_out_right);
+      textSwitcher.setInAnimation(in);
+      textSwitcher.setOutAnimation(out);
+    }
+
+    private void newText() {
+      index++;
+      if(index == totalTexts) {
+        index = 0;
+      }
+      textSwitcher.setText(texts[index]);
+      TextView nextView = (TextView)textSwitcher.getNextView();
+      TextView actualView = (TextView)textSwitcher.getCurrentView();
+      actualView.setTextColor(Color.parseColor(colors[index]));
+      nextView.setTextColor(Color.parseColor(colors[index]));
+    }
+
+    private void setFactory() {
+      textSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+        public View makeView() {
+          TextView myText = new TextView(context);
+          myText.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+          myText.setTextAppearance(context, R.style.textShadow);
+          return myText;
+        }
+      });
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+      while(!isCancelled()) {
+        try {
+          java.lang.Thread.sleep(700);
+        }
+        catch (InterruptedException e) {
+
+        }
+        publishProgress();
+        synchronized (pauseLock) {
+          while(paused) {
+            try {
+              pauseLock.wait();
+            }
+            catch (InterruptedException e) {
+
+            }
+          }
+        }
+      }
+      return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+      textSwitcher = findViewById(R.id.nameGame);
+      newText();
+    }
+
   }
 
 }
