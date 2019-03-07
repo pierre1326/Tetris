fbdb30640df4802d48d64fc4e4d9abe63b528402
pierre1326 <pierre1326@hotmail.com>
2019-03-06 20:49:05 -0600

    Cambios en el codigo
    Se añaden las imagenes de las piezas, ademas se elimina codigo innecesario ya que no es necesario rotar la pieza. Ademas se añade la clase Game que controlara la informacion del juego actual.

--- app/src/main/java/Game/Figure.java
+++ app/src/main/java/Game/Figure.java
@@ -41,40 +41,18 @@ public class Figure {
     else {
       form = (Square[][]) Util.rotateMatrixLeft(form, form.length, form[0].length);
     }
-    rotateSquares(90 * direction);
-  }
-
-  private void rotateSquares(int angle) {
-    for(int i = 0; i < form.length; i++) {
-      for(int j = 0; j < form[i].length; j++) {
-        form[i][j].rotate(angle);
-      }
-    }
   }
 
   public void update() {
-    updatePosition();
-    updateSquares();
-  }
-
-  public void move(int yF) {
-    if(!stoped && yF != y) {
-      this.yF = yF;
-    }
-  }
-
-  private void updatePosition() {
     if(!stoped) {
       this.x += velocity;
       this.y = Util.lerp(y, yF, dY);
     }
   }
 
-  private void updateSquares() {
-    for(int i = 0; i < form.length; i++) {
-      for(int j = 0; j < form[i].length; j++) {
-        form[i][j].update();
-      }
+  public void move(int yF) {
+    if(!stoped && yF != y) {
+      this.yF = yF;
     }
   }
 
---
+++ app/src/main/java/Game/Game.java
@@ -0,0 +1,24 @@
+package Game;
+
+public class Game {
+
+  private int score = 0;
+  private int level = 0;
+
+  public int getScore() {
+    return score;
+  }
+
+  public int getLevel() {
+    return level;
+  }
+
+  public Figure generateFigure() {
+    return null;
+  }
+
+  public Figure generateFigure(int type) {
+    return null;
+  }
+
+}
